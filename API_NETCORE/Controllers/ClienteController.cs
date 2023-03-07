d365d38facbf201e4817fdb336c58c931f3abb3f
MARCELO FRANCO <marcelo_dkp@3coracoes.com.br>
2023-03-07 16:51:18 -0300

    Incluindo projeto net core

---
+++ API_NETCORE/Controllers/ClienteController.cs
@@ -0,0 +1,55 @@
+﻿using Business;
+using Business.Interfaces;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_NETCORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+
+        IClienteBusiness _clienteBusiness = new ClienteBusiness();
+        // GET: api/<ClienteController>
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return _clienteBusiness.BuscarTodos();
+        }
+
+        // GET api/<ClienteController>/5
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+            return _clienteBusiness.BuscarPorId(id);
+        }
+
+        // POST api/<ClienteController>
+        [HttpPost]
+        public void Post([FromBody] Cliente cliente)
+        {
+            _clienteBusiness.Inserir(cliente);
+        }
+
+        // PUT api/<ClienteController>/5
+        [HttpPut("{id}")]
+        public void Put([FromBody] Cliente cliente)
+        {
+            _clienteBusiness.Atualizar(cliente);
+        }
+
+        // DELETE api/<ClienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _clienteBusiness.Excluir(id);
+        }
+    }
+}
